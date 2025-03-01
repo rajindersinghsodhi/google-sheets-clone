1a347b38d10140391dfe621e84f8308425434443
rajindersinghsodhi <rajsodhisocial@gmail.com>
2025-03-01 18:18:38 +0530

    Feature(header-controls): files control completed

--- src/app/util-components/spreadsheet-contols/file/file.component.ts
+++ src/app/util-components/spreadsheet-contols/file/file.component.ts
@@ -1,9 +1,11 @@
 import { Component, Input } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-file',
-  imports: [MatMenuModule],
+  imports: [MatMenuModule, MatIconModule, MatDividerModule],
   templateUrl: './file.component.html',
   styleUrl: './file.component.css'
 })
