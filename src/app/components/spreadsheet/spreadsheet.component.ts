e58b5120aa132b9ac354f0408f38cc1efc9518b8
rajindersinghsodhi <rajsodhisocial@gmail.com>
2025-03-01 14:39:52 +0530

    Feature(spreadsheet-controls): basic desing completed

--- src/app/components/spreadsheet/spreadsheet.component.ts
+++ src/app/components/spreadsheet/spreadsheet.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderSpreadsheetComponent } from "../../util-components/header-spreadsheet/header-spreadsheet.component";
+import { ControlsSpreadsheetComponent } from "../../util-components/controls-spreadsheet/controls-spreadsheet.component";
 
 @Component({
   selector: 'app-spreadsheet',
-  imports: [HeaderSpreadsheetComponent],
+  imports: [HeaderSpreadsheetComponent, ControlsSpreadsheetComponent],
   templateUrl: './spreadsheet.component.html',
   styleUrl: './spreadsheet.component.css'
 })
--- src/app/util-components/controls-spreadsheet/controls-spreadsheet.component.ts
+++ src/app/util-components/controls-spreadsheet/controls-spreadsheet.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-controls-spreadsheet',
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './controls-spreadsheet.component.html',
   styleUrl: './controls-spreadsheet.component.css'
 })
