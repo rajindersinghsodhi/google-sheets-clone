eca4f4a5fc126b4c9fbf3b0ddff5eb15caf51cc2
rajindersinghsodhi <rajsodhisocial@gmail.com>
2025-03-01 13:53:39 +0530

    Feature(header): header design completed

--- src/app/components/spreadsheet/spreadsheet.component.ts
+++ src/app/components/spreadsheet/spreadsheet.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { HeaderSpreadsheetComponent } from "../../util-components/header-spreadsheet/header-spreadsheet.component";
 
 @Component({
   selector: 'app-spreadsheet',
-  imports: [],
+  imports: [HeaderSpreadsheetComponent],
   templateUrl: './spreadsheet.component.html',
   styleUrl: './spreadsheet.component.css'
 })
--- src/app/util-components/header-spreadsheet/header-spreadsheet.component.ts
+++ src/app/util-components/header-spreadsheet/header-spreadsheet.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-header-spreadsheet',
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './header-spreadsheet.component.html',
   styleUrl: './header-spreadsheet.component.css'
 })
